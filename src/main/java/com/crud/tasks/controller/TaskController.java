b7a4008d5dca737b809e571e292896983a5d63d8
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-04-17 21:36:22 +0200

    15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,21 +1,35 @@
-package com.crud.tasks.domain.controller;
+package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import com.crud.tasks.domain.Task;
 import java.util.ArrayList;
 import java.util.List;
-
-
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/v1/tasks")
 public class TaskController {
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @Autowired
+    public TaskController(DbService service, TaskMapper taskMapper) {
+        this.service = service;
+        this.taskMapper = taskMapper;
+    }
+
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
+
 
     @GetMapping("/{taskId}")
     public TaskDto getTask(@PathVariable Long taskId) {
--- src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
+++
@@ -1,2 +0,0 @@
-package com.crud.tasks.controller;public class TaskNotFoundException {
-}
